4b4bbe2bbef21855050c5bc1cf68deb41540f33c
unknown <najehtoumi2@gmail.com>
2015-05-30 17:02:01 +0200

    ListDoctor

---
+++ src/main/java/model/ListDoctor.java
@@ -0,0 +1,15 @@
+package model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListDoctor {
+	public List<Doctor> lDoctor = new ArrayList<Doctor>();
+	
+	
+
+	  public void add( Doctor D){
+	      lDoctor.add(D); 
+	   }
+
+}
